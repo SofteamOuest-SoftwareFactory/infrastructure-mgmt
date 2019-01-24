04180ceeb1e717921afe9c960e6f0ac1308ec929
EL KOUHEN Mehdi <mehdi.elkouhen@gmail.com>
2019-01-24 22:34:51 +0100

    update infra

--- index.js
+++ index.js
@@ -15,31 +15,30 @@ var partitionCreate = require('./partition-create');
 var partitionAddAccess = require('./partition-add-access');
 var taskStatus = require('./task-status');
 
-let VPSs = ["vps242130.ovh.net", 
+
+let VPSs = [
     "vps242131.ovh.net",
-    "vps242564.ovh.net", 
-    "vps242565.ovh.net",
-    "vps267690.ovh.net", 
-    "vps267694.ovh.net", 
-    "vps257315.ovh.net"];
+    "vps242130.ovh.net",
+    "vps257315.ovh.net",
+    "vps568063.ovh.net",
+    "vps568065.ovh.net"];
 
-let IPs = ["149.202.62.219", 
-    "149.202.62.97", 
-    "51.254.143.249", 
-    "51.254.143.43",
-    "92.222.81.117", 
-    "51.255.200.197", 
-    "51.255.42.138"];
+let IPs = ["149.202.62.97",
+    "149.202.62.219",
+    "51.255.42.138",
+    "51.38.238.172",
+    "51.38.238.181"];
 
-let partitions = ["WWWELASTIC"];
-
-if (false) ovh.request('GET', "/me", function (err, data) {
-    console.log(err || 'Me ' + JSON.stringify(data));
-});
+let partitions = ["WWWELASTIC",
+    "WWWLOGSTASH",
+    "WWWSONARQUBE",
+    "WWWJENKINS",
+    "WWWNEXUS"];
 
 program
     .version('0.1.0')
     .description('OVH Infrastructure Management')
+    .option('-gs, --gs', 'Get Me Status')
     .option('-ts, --ts', 'Task Status')
     .option('-vs, --vs', 'VPS Status')
     .option('-vr, --vr', 'VPS Reinstall')
@@ -47,6 +46,12 @@ program
     .option('-pc, --pc', 'Partition Create')
     .option('-pa, --pa', 'Partition Add Access')
     .parse(process.argv);
+
+if (program.gs) {
+    ovh.request('GET', "/me", function (err, data) {
+        console.log(err || JSON.stringify(data));
+    });
+}
 
 if (program.ts) {
     taskStatus(ovh);
